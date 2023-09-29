9a48b456bada546827e042b4e76a3c04a744f476
Hendrick Montaril <146336261+HendrickMontaril@users.noreply.github.com>
2023-09-30 07:59:07 +0800

    Add files via upload

---
+++ experiment01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string madara;
+
+cout<<"Enter your name: ";
+getline (cin,madara);
+
+cout<<"The length of your name is: "<<madara[3];
+
+return 0;
+
+}
