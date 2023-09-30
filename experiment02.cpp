f67d42386ec778622216a5ccd7662f6fa19ced87
Hendrick Montaril <146336261+HendrickMontaril@users.noreply.github.com>
2023-09-30 08:22:54 +0800

    Add files via upload

---
+++ experiment02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string uchiha, godlyghost = "\"GOD\"";
+
+cout<<"Enter your name: ";
+getline (cin,uchiha);
+
+cout<<"The length of your name is: "<<uchiha.size();
+cout<<"\nYour are: "<<godlyghost;
+
+return 0;
+
+}
